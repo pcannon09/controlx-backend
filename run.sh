d9f80c6dcff3e45bd66915d039f8acdc057aecae
pcannon09 <pcannon775@gmail.com>
2026-02-09 18:46:57 +0100

    Add license printing param

--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
-python -m main
+readonly LICENSE="ControlX-Backend (C) 2026 pcannon09
+This program comes with ABSOLUTELY NO WARRANTY; for details type \`./run.sh --license\`.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type \`./run.sh --license\` for details."
+
+if [[ "$1" == "--license" ]]; then
+	echo "$LICENSE"
+
+else
+	python -m main
+fi
